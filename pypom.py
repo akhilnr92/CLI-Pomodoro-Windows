ce5c69ce96165fbba02c001c709dd54588b4c11b
akhilnr92 <66834943+akhilnr92@users.noreply.github.com>
2023-01-31 00:15:57 +0530

    Add files via upload

---
+++ pypom.py
@@ -0,0 +1,27 @@
+import subprocess
+import time
+import ctypes
+import sys
+import os
+
+def pomodoro(val, duration):
+    print('\n > '+val+'\n')
+    subprocess.run(["timer.exe", str(duration * 60)])
+    ctypes.windll.user32.MessageBoxW(None, "'{}' session done".format(val), "Pomodoro Timer", 0x40 | 0x1)
+    print("'{}' session done".format(val))
+
+def main():
+    work_duration = int(input("Enter Work session : "))
+    break_duration = int(input("Enter Break session : "))
+    try:
+        while True:
+            pomodoro("WORK", work_duration)
+            os.system("cls" if os.name == "nt" else "clear")
+            pomodoro("BREAK", break_duration)
+            os.system("cls" if os.name == "nt" else "clear")
+    except KeyboardInterrupt:
+        print("Exiting...")
+        sys.exit(0)
+
+if __name__ == '__main__':
+    main()
